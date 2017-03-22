d1b791be3abc9be6bbfd55b6bac5d471a90bfc5c
Luiz Marcelo Lopes Costa Junior <marcardion@gmail.com>
2017-03-21 22:21:19 -0400

    Creating the Repo

---
+++ Assets/Ship_Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Movement : MonoBehaviour {
+
+	Rigidbody myRigidbody;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		myRigidbody.velocity = new Vector3 (moveHorizontal * speed, myRigidbody.velocity.y, moveVertical * speed);
+
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x, -10.49f, 11.75f),
+			transform.position.y,
+			Mathf.Clamp (transform.position.z, -3.3f, 8.7f));
+		
+	}
+}
